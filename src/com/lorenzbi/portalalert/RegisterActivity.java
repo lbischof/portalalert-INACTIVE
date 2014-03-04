498332c4dfe3c32952d484178058dd2229559c29
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-04 22:08:10 +0100

    send data to server

--- src/com/lorenzbi/portalalert/RegisterActivity.java
+++ src/com/lorenzbi/portalalert/RegisterActivity.java
@@ -1,7 +1,18 @@
 package com.lorenzbi.portalalert;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.content.Context;
@@ -46,7 +57,7 @@ ConnectionCallbacks, OnConnectionFailedListener, OnClickListener {
     AtomicInteger msgId = new AtomicInteger();
     SharedPreferences prefs;
     Context context;
-
+    String username;
     String regid;
 
 
@@ -135,7 +146,7 @@ ConnectionCallbacks, OnConnectionFailedListener, OnClickListener {
 	@Override
 	public void onConnected(Bundle connectionHint) {
 		final EditText usernameInput = (EditText)findViewById(R.id.username);
-		String username = usernameInput.getText().toString();
+		username = usernameInput.getText().toString(); //TODO do something with this
 		
 		String personName = "";
 		String email = "";
@@ -251,7 +262,27 @@ ConnectionCallbacks, OnConnectionFailedListener, OnClickListener {
      * using the 'from' address in the message.
      */
     private void sendRegistrationIdToBackend() {
-        // TODO: send regid to server
+    	HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://www.lorenzz.ch/portalalert.php");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+            nameValuePairs.add(new BasicNameValuePair("username", username));
+            nameValuePairs.add(new BasicNameValuePair("regid", regid));
+            nameValuePairs.add(new BasicNameValuePair("name", ""));
+            nameValuePairs.add(new BasicNameValuePair("email", ""));
+
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
     
     /**
@@ -308,7 +339,7 @@ ConnectionCallbacks, OnConnectionFailedListener, OnClickListener {
 	}
 
 	public void onDisconnected() {
-		// TODO Auto-generated method stub
+		
 	}
 	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
 		if (requestCode == RC_SIGN_IN) {
