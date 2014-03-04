86a06aac382dbd5e3735fb5a9788460ad2cf20e1
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-04 17:26:48 +0100

    added google login

---
+++ src/com/lorenzbi/portalalert/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lorenzbi.portalalert;
+
+import com.lorenzbi.nr01golf07.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	// label to display gcm messages
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+}
