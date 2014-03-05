66d4e7d842565b20df7051239d7bde054f4adeb5
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-05 20:26:12 +0100

    trying to add http post handling

--- server/app.js
+++ server/app.js
@@ -18,13 +18,24 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(express.json());   
 app.use(bodyParser.urlencoded());
+app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 app.get('/', routes.index);
 app.get('/users', users.list);
+
+app.post('/register', function(req, res) {
+    var regid = req.body.regid,
+        name = req.body.name,
+        email = req.body.email,
+        username = req.body.username;
+    console.log(regid);
+});
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
