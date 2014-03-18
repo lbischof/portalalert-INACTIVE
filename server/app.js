1707ade4d3a379c3c06e7d29020b3d2ec13878ae
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-18 15:30:34 +0100

    undefined

--- server/app.js
+++ server/app.js
@@ -59,7 +59,6 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	console.log(err);
     res.render('error', {
         message: err.message,
         error: err
