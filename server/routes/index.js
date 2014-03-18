6bf993b119efde00a608cdcbf7e0fe2505e1b0f0
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-18 15:37:10 +0100

    undefined

--- server/routes/index.js
+++ server/routes/index.js
@@ -53,8 +53,8 @@ exports.alert = function(db) {
     var users = db.get('users');
 
     // Submit to the DB
-    users.find({}, {'regid' : 1}).toArray(function(err, docs){
-    //console.log(docs);
+    users.find({}, {'regid' : 1}, function(err, docs){
+    console.log(docs);
 });
     alerts.insert({
     	"lat" : lat,
