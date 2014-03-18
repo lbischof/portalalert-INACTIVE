532a4f02f6bb9ace4f5ad1495c36fbfe9763bf38
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-18 15:36:03 +0100

    undefined

--- server/app.js
+++ server/app.js
@@ -55,7 +55,9 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
+});
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
