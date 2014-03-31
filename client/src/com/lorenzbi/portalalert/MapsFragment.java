730ea8fd4768e35dbb63c6a168d291b89d19c70d
Lorenz Bischof <lorenzbischof@gmail.com>
2014-03-31 23:24:39 +0200

    fixed map fragment

--- client/src/com/lorenzbi/portalalert/MapsFragment.java
+++ client/src/com/lorenzbi/portalalert/MapsFragment.java
@@ -1,60 +1,61 @@
 package com.lorenzbi.portalalert;
 
 import android.app.Fragment;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 
 public class MapsFragment extends Fragment {
 	
-		static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-        static final LatLng KIEL = new LatLng(53.551, 9.993);
-        private GoogleMap map;
-        private static View view;
+	private GoogleMap mMap;
+	private static View view;
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	        Bundle savedInstanceState) {
+		if (view != null) {
+	        ViewGroup parent = (ViewGroup) view.getParent();
+	        if (parent != null)
+	            parent.removeView(view);
+	    }
+	    try {
+	        view = inflater.inflate(R.layout.fragment_map, container, false);
+	    } catch (InflateException e) {
+	        /* map is already there, just return view as it is */
+	    }
+	    setUpMapIfNeeded();
+	    return view;	     
+	}
 
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-	  Log.d("oncreateview", "oncreateview");
-	  try {
-      view = (RelativeLayout) inflater.inflate(R.layout.fragment_map, container, false);
-	  } catch(Exception e) {
-		  
-	  }
-	  
-	      map = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapview))
-	          .getMap();
-	      map.setMyLocationEnabled(true);
-	      
-	Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-	              .title("Hamburg"));
-	          Marker kiel = map.addMarker(new MarkerOptions()
-	              .position(KIEL)
-	              .title("Kiel")
-	              .snippet("Kiel is cool")
-	              .icon(BitmapDescriptorFactory
-	                  .fromResource(R.drawable.ic_launcher)));
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    setUpMapIfNeeded();
+	}
 
-	          // Move the camera instantly to hamburg with a zoom of 15.
-	          map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+	private void setUpMapIfNeeded() {
+	    // Do a null check to confirm that we have not already instantiated the map.
+	    if (mMap == null) {
+	        // Try to obtain the map from the SupportMapFragment.
+	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapview)).getMap();
+	        // Check if we were successful in obtaining the map.
+	        if (mMap != null) {
+	            setUpMap();
+	        }
+	    }
+	}
 
-	          // Zoom in, animating the camera.
-	          map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-	  
-      return view;
-  }
- 
-}
+	private void setUpMap() {
+	      mMap.setMyLocationEnabled(true);
+	    mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+	}
+	
+	}
