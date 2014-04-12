43bd02a9b01154e51f78a48593536fcf83bc1d4b
Lorenz Bischof <lorenzbischof@gmail.com>
2014-04-12 15:16:15 +0200

    undefined

--- server/routes/index.js
+++ server/routes/index.js
@@ -108,10 +108,10 @@ exports.done = function(db) {
 
 		alerts.update({"_id":id},{ $set: { "done" : true }}, function(err, numAffected){
 			if(numAffected == 1){
-				res.send(id);
 				users.distinct('regid',{location: {$near : { $geometry : { type: "Point", coordinates : [ lng, lat ]}, $maxDistance : 3000}}},function(err, docs){
+					res.send(id);
 					registrationIds = docs;
-					/*var gcm = require('node-gcm');
+					var gcm = require('node-gcm');
 					var gcmMessage = new gcm.Message({
 					//collapseKey: 'demo',
 						data: {"done":id}
@@ -120,7 +120,7 @@ exports.done = function(db) {
 				
 				sender.send(gcmMessage, registrationIds, 4, function (err, result) {
 					console.log(result);
-				});*/
+				});
 			});
 			}
 		});
