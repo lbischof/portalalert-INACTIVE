0f94d860cf0a6fb9b1ec9da7ac9ea7ec510c8dd4
Lorenz Bischof <lorenzbischof@gmail.com>
2014-04-20 20:23:41 +0200

    undefined

--- server/app.js
+++ server/app.js
@@ -40,6 +40,7 @@ app.post('/alert', routes.alert(db));
 app.post('/sync', routes.sync(db));
 app.post('/done', routes.done(db));
 app.post('/upload', routes.upload(db));
+app.post('/search', routes.search(db));
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
