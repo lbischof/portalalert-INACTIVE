a7e1809a8ce5801b896ce22fd68043e687eb293a
Lorenz Bischof <lorenzbischof@gmail.com>
2014-04-21 20:12:23 +0200

    undefined

--- server/public/iitc-portalalert.user.js
+++ server/public/iitc-portalalert.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             ingress-portalalert
 // @name           PortalAlert
-// @version        8
+// @version        9
 // @namespace      https://github.com/Lorenzbi/portalalert
 // @downloadURL    http://portalalert.lorenzz.ch:3000/iitc-portalalert.user.js
 // @updateURL      http://portalalert.lorenzz.ch:3000/iitc-portalalert.user.js
