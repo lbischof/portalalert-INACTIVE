333a1cba14eafe9f274b918de62f9bc699ef32cc
Lorenz Bischof <lorenzbischof@gmail.com>
2014-04-21 15:34:28 +0200

    undefined

--- server/routes/index.js
+++ server/routes/index.js
@@ -223,7 +223,6 @@ exports.upload = function(db) {
         var portals = db.get('portals');
         console.log(portals);
         portals.ensureIndex( { "location" : "2dsphere" } );
-        portals.ensureIndex( { title: 1 }, { unique: true } );
 
         for (var i = portalsuploaded.length - 1; i >= 0; i--) {
             portalsuploaded[i].location.coordinates[0] = parseFloat(portalsuploaded[i].location.coordinates[0]);
